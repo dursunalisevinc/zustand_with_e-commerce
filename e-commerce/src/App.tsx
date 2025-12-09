0c1691ba7b76edc87cb8ab8b2043bbfe7cab1552
Dursun Ali Sevinç <dursun.sevinc@detaysoft.com>
2025-12-09 17:45:00 +0300

    react router dom added

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Products from './pages/products/Products';
+import ProductsDetails from './pages/details/ProductsDetails';
 
 function App() {
 
   return (
     <>
-      <div className='bg-red-300'>
-        Zustand e-commerce Start
-      </div>
+      <BrowserRouter>
+        {/* Navigation */}
+        <nav>
+          <Link to="/">dashboard</Link> |{" "}
+          <Link to="/:id">details</Link> |{" "}
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/:id" element={<ProductsDetails />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
