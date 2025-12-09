38cd33f2e4b87dc1a4b80b8c51b7910c3dd38cb7
Dursun Ali Sevinç <dursun.sevinc@detaysoft.com>
2025-12-09 16:31:12 +0300

    talwind and vite created

---
+++ e-commerce/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-300'>
+        Zustand e-commerce Start
+      </div>
+    </>
+  )
+}
+
+export default App
